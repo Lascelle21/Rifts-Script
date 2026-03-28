8db46a3383cf078d26462053031afeb719457ea3
Lascelle21 <60103293+Lascelle21@users.noreply.github.com>
2026-03-28 00:56:03 +0000

    experimenting with portal interaction
    Not too sure yet why the order to reassign transform position for the character's body won't work. leading to the camera pivot to change and player position to be incorrect

--- PortalScript.cs
+++ PortalScript.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PortalScript : MonoBehaviour
 {
+    public GameObject portaL1;
+    public GameObject portaL2;
+    public GameObject[] PlayeRComponents;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,22 @@ public class PortalScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //print(portaL2.transform.position);
+       // print(PlayeR.transform.position+ "player");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.tag == "portal1")
+        {
+            if (other.tag == "Player")
+            {
+                PlayeRComponents[0].transform.position = portaL2.transform.position;
+                PlayeRComponents[1].transform.position = portaL2.transform.position;
+                PlayeRComponents[2].transform.position = portaL2.transform.position;
+                PlayeRComponents[3].transform.localPosition = portaL2.transform.position;
+
+            }
+        }
+        //
     }
 }
