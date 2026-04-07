049c84dc5e127358449cb06a99f2d38afcda2b83
Lascelle21 <60103293+Lascelle21@users.noreply.github.com>
2026-04-07 17:05:14 +0100

    found fix to being trapped in between portals
    added a wait for seconds to allow the player to escape second portals collider moving onto trajectory behaviour when exiting at different angles

--- PortalScript.cs
+++ PortalScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PortalScript : MonoBehaviour
@@ -6,53 +7,65 @@ public class PortalScript : MonoBehaviour
     public GameObject portaL2;
     public GameObject PlayeR;
     public bool allClear = true;
+    WaitForSeconds shifTing;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        shifTing = new WaitForSeconds(0.1f); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //print(PlayeR.transform.position);
+
         //print(portaL2.transform.position);
         // print(PlayeR.transform.position+ "player");
+        //RaycastHit hit;
+        //Physics.Raycast(portaL1.transform.position, portaL1.transform.forward, out hit);
+        //Quaternion.LookRotation(hit.normal);
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (this.tag == "portal1")
+        StartCoroutine(portaldelaY());
+        if (tag == "portal1")
         {
-            if (other.transform.position.y >= this.transform.position.y)
+            if (other.tag == "Player" && other.transform.position.y >= this.transform.position.y)
             {
                 allClear = false;
                 PlayeR.GetComponent<Rigidbody>().linearVelocity = PlayeR.GetComponent<playerMotioncontrol>().reversemomentum;
-
-                print("going up");
-            }
-            if (other.tag == "Player")
-            {
-                PlayeR.transform.position = portaL2.transform.position;                
+                PlayeR.transform.position = portaL2.transform.position;
+                portaL2.GetComponent<MeshCollider>().enabled = false;
+                portaldelaY();
             }
         }
-        if (this.tag == "portal2")
+        if (tag == "portal2")
         {
-            if (other.transform.position.y >= this.transform.position.y)
+            if (other.tag == "Player" && other.transform.position.y >= this.transform.position.y)
             {
                 allClear = false;
                 PlayeR.GetComponent<Rigidbody>().linearVelocity = PlayeR.GetComponent<playerMotioncontrol>().reversemomentum;
-
-                print("going up");
-            }
-            if (other.tag == "Player")
-            {
+                portaL1.GetComponent<MeshCollider>().enabled = false;
                 PlayeR.transform.position = portaL1.transform.position;
+                portaldelaY();
             }
         }
     }
+    IEnumerator portaldelaY()
+    {
+        yield return shifTing;
+        print("past");
+        portaL1.GetComponent<MeshCollider>().enabled = true;
+        portaL2.GetComponent<MeshCollider>().enabled = true;
+        allClear = true;
+    }
+
+
+
     private void OnTriggerExit(Collider other)
     {
 
-        allClear = true;
+        
     }
 }
 
